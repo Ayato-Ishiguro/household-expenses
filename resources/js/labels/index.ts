50f8330d75afaa90008fedecb95f85e6b5c42118
ayato.ishiguro <sabitayoroi@gmail.com>
2025-11-07 21:35:59 +0900

    chore

--- resources/js/labels/index.ts
+++ resources/js/labels/index.ts
@@ -1,6 +1,6 @@
 import en from "@/Labels/auth/en";
 import ja from "@/Labels/auth/ja";
-import { Labels } from "./types";
+import { Labels } from "@/Labels/types";
 
 const lang = navigator.language.startsWith("ja") ? "ja" : "en";
 const labels: Labels = lang === "ja" ? ja : en;
